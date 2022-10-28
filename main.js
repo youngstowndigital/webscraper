63bf33bd9739fe02432137e8c9a555127fe02e3c
Mike Wittenauer <mikewittenauer@me.com>
2022-10-28 08:11:57 -0400

    Add data worker message sending to main thread

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
+const { Worker }  = require('worker_threads');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const url = 'https://www.iban.com/exchange-rates';
-
-fetchData(url).then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
-    statsTable.each(function() {
-        let title = $(this).find('td').text();
-        console.log(title);
-    })
-});
 
 async function fetchData(url){
     console.log("Crawling data...")
@@ -22,4 +12,46 @@ async function fetchData(url){
         return;
     }
     return response;
+}
+
+let workDir = __dirname+"/dbWorker.js";
+
+const mainFunc = async () => {
+    const url = "https://www.iban.com/exchange-rates";
+    let res = await fetchData(url);
+    if (!res.data) {
+        console.log("Invalid data obj");
+        return;
+    }
+    const html = res.data;
+    let dataObj = new Object();
+
+    const $ = cheerio.load(html);
+
+    const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
+    statsTable.each(function() {
+        let title = $(this).find('td').text();
+        let newStr = title.split("\t");
+        newStr.shift();
+        formatStr(newStr, dataObj);
+    });
+
+    return dataObj;
+}
+
+mainFunc().then((res) => {
+    const worker = new Worker(workDir);
+    console.log("Sending crawled data to dbWorker...");
+
+    worker.postMessage(res);
+
+    worker.on("message", (message) => {
+        console.log(message);
+    });
+});
+
+function formatStr(arr, dataObj) {
+    let regExp = /[^A-Z]*(^\D+)/ 
+    let newArr = arr[0].split(regExp);
+    dataObj[newArr[1]] = newArr[2];
 }
